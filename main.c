929c336205a95d6a14fd7d0a22ed7cd3e1c55cad
kawaharasouta <kawahara6514@gmail.com>
2018-01-30 14:05:10 +0900

    add comment

--- main.c
+++ main.c
@@ -5,8 +5,11 @@ typedef void (*signalhandler_t)(int);
 sighandler_t trap_signal(int sig, sighandler_t handler){
 	struct sigaction act, old;
 
+	//set signal handler. ignore sa_sigaction
 	act.sa_handler = handler;
+	//empty sa_mask
 	sigemptyset(&act.sa_mask);
+	//restart setting
 	act.sa_flags = SA_RESTART;
 
 	if (sigaction(sig, &act, &old) < 0) return NULL;
