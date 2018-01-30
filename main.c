57e48de39fb1f31f2a54d34bc4c5cac37d28e430
kawaharasouta <kawahara6514@gmail.com>
2018-01-30 13:55:53 +0900

    update

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<signal.h>
+
+typedef void (*signalhandler_t)(int);
+
+sighandler_t trap_signal(int sig, sighandler_t handler){
+	struct sigaction act, old;
+
+	act.sa_handler = handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_RESTART;
+
+	if (sigaction(sig, &act, &old) < 0) return NULL;
+	return old.sa_handler;
+}
